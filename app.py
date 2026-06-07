aff40f18d6b425fec4becec9b60569bc10a2cc06
KumarYash07 <yash.23b0101151@abes.ac.in>
2026-06-07 14:10:56 +0530

    Di vercel

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pickle
 import numpy as np
 import os
@@ -6,6 +7,7 @@ import sys
 
 # ================= APP CONFIG =================
 app = Flask(__name__)
+CORS(app) 
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 MODEL_PATH = os.path.join(BASE_DIR, "model.pkl")
